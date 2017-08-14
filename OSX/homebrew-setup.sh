b4953543d6d49695048a5ea4830691a069e93280
Chris Woollard <cwoollard@gmail.com>
2017-08-14 09:16:18 +0100

    Create homebrew-setup.sh

---
+++ OSX/homebrew-setup.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+# Brew packages that I use alot.
+#
+brew install awscli
+brew install cask
+brew install fish
+brew install htop
+brew install imagemagick
+brew install python3
+brew install wakeonlan
+brew install wget
+
+#
+# Brew packages that I use for Pen Testing
+#
+brew install nmap
+brew install sslyze
+brew install sslscan
+brew install testssl
+
+#
+# Some cask packages that I like.
+#
+brew cask install sublime-text
+brew cask install alfred
